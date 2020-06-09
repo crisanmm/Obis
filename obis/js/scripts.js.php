c35a667aef09af4af66caa3b73436c8ef535bcd3
HMM <mhristodor@bitdefender.com>
2020-06-09 23:16:13 +0200

    fixed navbar

--- obis/js/scripts.js.php
+++ obis/js/scripts.js.php
@@ -21,3 +21,32 @@ function setCookie(cname, cvalue, exdays) {
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
+function leSizeCheck()
+{
+	var ul = document.getElementById("headDrop");
+
+	if(window.innerWidth >= 769)
+	{
+		if(document.getElementById("tmp"))
+		{
+			var li = document.getElementById("tmp");
+			ul.removeChild(li);
+		}
+	}else if (!document.getElementById("tmp"))
+	{
+		var li = document.createElement("li");
+		
+		li.setAttribute('id',"tmp");
+		ul.appendChild(li);
+
+		document.getElementById("tmp").innerHTML = `<a href="/obis/account/user" class="nav-button">
+                        <?php echo isset($_SESSION['firstname']) ? $_SESSION['firstname'] : "LOG IN" ?>
+                    </a>`;
+	}
+}
+
+window.onload = function()
+{
+	leSizeCheck();
+}
